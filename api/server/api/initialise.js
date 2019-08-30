73af760af3704ecfc6b9da118c3c7c2cb21bdf0d
Josh Spindler <joshuaspindler@outlook.com>
2019-08-30 13:27:30 +0100

    Refactored some code

--- api/server/api/initialise.js
+++ api/server/api/initialise.js
@@ -6,30 +6,35 @@ import {isDev} from "../environment";
 import generateRoutes from "./routes/v1/generate-routes";
 
 // need redis, swagger
-// clean all of this up and make into functions, not defined in this function
 export default function initialiseServer() {
     const app = express();
     const port = process.env.APP_PORT || 3000;
 
     app.use(morgan(isDev ? 'dev' : null));
     app.use(bodyParser.json({limit: '50mb'}));
-    app.use((req, _, next) => {
-        logger.info(`${req.method} request received at ${req.url}`);
-        next();
-    });
-    app.use((_, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
-        next()
-    });
+    app.use(logRequest);
+    app.use(setHeaders);
     app.use('/v1', generateRoutes);
 
-    process.on('uncaughtException', (err) => {
-        logger.error(`uncaught exception: ${err}`);
-    });
+    process.on('uncaughtException', logException);
 
     app.listen(port, () => {
         logger.info(`faceswap API ready to go on port ${port} 👍`);
     });
+};
+
+const setHeaders = (_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+    next()
+};
+
+const logRequest = (req, _, next) => {
+    logger.info(`${req.method} request received at ${req.url}`);
+    next();
+};
+
+const logException = (err) => {
+    logger.error(`uncaught exception: ${err}`);
 };
